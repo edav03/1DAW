b76c3c09c04c929f3d94533d48d352248816136e
edav03 <edavalc03@hotmail.com>
2021-09-30 10:59:43 +0200

    Actualizacion Clases

---
+++ Programacion/U2_IntroJava/Practicas/prueba.java
@@ -0,0 +1,11 @@
+public class prueba{
+    public static void main(String[] args){
+        int edad;
+
+        for(edad = 0; edad < 100000; edad++){
+            System.out.println("Tienes " + edad + " años");
+        }
+    }
+
+
+}_
