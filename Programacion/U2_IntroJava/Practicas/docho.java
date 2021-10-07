cd7e5ff84e54a2d75d2a0a19636533d2213dbe41
edav03 <edavalc03@hotmail.com>
2021-10-07 19:57:50 +0200

    Actualizacion Clases

--- Programacion/U2_IntroJava/Practicas/docho.java
+++ Programacion/U2_IntroJava/Practicas/docho.java
@@ -4,10 +4,11 @@ public class docho {
     public static void main(String[] arg){
         Scanner hours = new Scanner(System.in);
         int h, p;
-        double salary, y, n, r;
-        int t_normal = 9;
+        double salary = 0;
+        double y, n;
+        int t_normal = 10;
 
-        System.out.println("(La tarifa normal se paga a 9€ la hora)");
+        System.out.println("(La tarifa normal se paga a 10€ la hora)");
         System.out.print("Introduce las horas trabajadas: ");
         h = hours.nextInt();
 
@@ -16,25 +17,28 @@ public class docho {
         }
         else{
             p = h - 35;
-            y = (35 * t_normal) + (p * (1.5 * t_normal));
+            y = (p * (1.5 * t_normal)+ 350);
         }
 
 
-        // if (y < 501){
-        //     y = salary;
-        //     System.out.println("Salario = " + salary);
-        // }
-        // else{
-        //     if(y < 901){
-        //         n = y - 500;
-        //         y = 500 + n;
-        //         salary = 500 + (n + 0.25);
-        //     }
-        //     else{
-        //         n = 100 + r;
-        //         y = n - 100;
-        //     }
-        // }
+        if (y < 501){
+            salary = y;
+            System.out.println("Salario = " + salary);
+        }
+        else{
+            if(y < 901 && y > 500){
+                n = y - 500;
+                salary = 500 + (n * 0.25);
+                System.out.println("Salario correspondiente = " + salary);
+            }
+            else{
+                n = y - 900;
+                salary = 600 + (n * 0.45);
+                System.out.println("Salario correspondiente = " + salary);
+            }
+        }
+
+        hours.close();
         
     }
 }
