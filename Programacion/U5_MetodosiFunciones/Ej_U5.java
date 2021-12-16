606eb4ecf57f7dd74d40d322a2be0dbd5eb8998d
Vidaved <edavalc03@hotmail.com>
2021-12-16 20:48:46 +0100

    Actualizacion Clases

--- Programacion/U5_MetodosiFunciones/Ej_U5.java
+++ Programacion/U5_MetodosiFunciones/Ej_U5.java
@@ -3,19 +3,29 @@ import java.util.Scanner;
 public class Ej_U5{
 
     public static void main(String[] args){
-        Scanner num = new Scanner(System.in);
+        int arroy[] = new int[100];
 
-        System.out.print("Introduce la cantidad sin descuento: ");
-        double n1 = num.nextDouble();
+        for(int i = 0; i < arroy.length; i++){
+            arroy[i] = i + 1;
+        }
 
-        System.out.print("Introduce la cantidad con descuento: ");
-        double n2 = num.nextDouble();
-
-        double result = desc(n1, n2);
-
+        int s = suma(arroy[]);
+        double m = media(arroy[]);
         
     }
 
-    public static double desc(double n1, double n2){
+    public static int suma(int a[]){
+        int plus = 0;
+
+        for(int i = 0; i < a.length; i++){
+            plus += a[i];
+        }
+
+        return plus;
+    }
+
+    public static double media(int b[]){
+        int media = 0;
+        return media;
     }
 }
