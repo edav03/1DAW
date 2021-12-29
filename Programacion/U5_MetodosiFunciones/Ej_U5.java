7f8012868025e66d22221b04b157e372983e9a30
edav03 <edavalc03@hotmail.com>
2021-12-29 17:37:48 +0100

    Actualizacion Clases

--- Programacion/U5_MetodosiFunciones/Ej_U5.java
+++ Programacion/U5_MetodosiFunciones/Ej_U5.java
@@ -3,37 +3,73 @@ import java.util.Scanner;
 public class Ej_U5{
 
     public static void main(String[] args){
-       Scanner num = new Scanner(System.in);
+        int n = -1;
 
-       System.out.print("Introduce X: ");
-       int x = num.nextInt();
+        while(n != 0){
+            n = menu();
 
-       System.out.print("Introduce Y: ");
-       int y = num.nextInt();
+            double radio = pideRadio();
 
-       System.out.print("Introduce Z: ");
-       int z = num.nextInt();
+            switch(n){
+                case 1:
+                    double r = circunferencia(radio);
+                    System.out.println("Circunferencia = " + r);
+                break;
+                case 2:
+                    double ra = area(radio);
+                    System.out.println("Area = " + ra);
+                break;
+                case 3:
+                    double v = volumen(radio);
+                    System.out.println("Volumen = " + v);
+                break;
+                case 4:
+                    double c = circunferencia(radio);
+                    double ar = area(radio);
+                    double vol = volumen(radio);
+                    System.out.println("Circunferencia = " + c + "\nArea = " + ar + "\nVolumen = " + vol);
+                break;
+            }
 
-       boolean res = formula(x, y, z);
-
-       if(res == true){
-            System.out.println("Son iguales");
-       }
-       else{
-           System.out.println("No son iguales");
-       }
+        }
     }
 
-    public static boolean formula(int x, int y, int z){
-        boolean res =  true;
+    public static int menu(){
+        Scanner num = new Scanner(System.in);
+        
+        System.out.println("Menú\n0.Salir\n1.Circunferencia\n2.Area\n3.Volumen\n4.Todas");
+        int x = num.nextInt();
 
-        if((Math.pow(x, 2) + Math.pow(y, 2) == Math.pow(z, 2))){
-            res = true;
-        }
-        else{
-            res = false;
-        }
+        return x;
 
-        return res;
+    }
+
+    public static double pideRadio(){
+        Scanner num = new Scanner(System.in);
+
+        System.out.print("Introduce el radio: ");
+        double radio = num.nextDouble();
+
+        return radio;
+    }
+
+    public static double circunferencia(double r){
+        double circ = 2*Math.PI*r;
+
+        return circ;
+
+    }
+
+    public static double area(double r){
+        double ar = Math.PI * Math.pow(r, 2);
+
+        return ar;
+
+    }
+
+    public static double volumen(double r){
+        double vol = (4/3) * Math.PI * Math.pow(r, 3);
+
+        return vol;
     }
 }
