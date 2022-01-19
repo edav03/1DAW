bc680687505fbf6a2aa72dca9439ace176106717
Vidaved <edavalc03@hotmail.com>
2022-01-19 18:28:03 +0100

    Actualizacion Clases

---
+++ Programacion/U6_ProgamacionObjectes/Act2_3/Actividad2.java
@@ -0,0 +1,6 @@
+public class Actividad2{
+    public static void main(String[] args){
+        Cotxe c1;
+        c1 = new Cotxe();
+    }
+}
---
+++ Programacion/U6_ProgamacionObjectes/Act2_3/Cotxe.java
@@ -0,0 +1,11 @@
+public class Cotxe {
+
+    public String model;
+    public String color;
+    public boolean met;
+    public int matricula;
+    public char cotxe;
+    public int fabricacion;
+    public char modalidad;
+
+}
