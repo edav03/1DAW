4b60b941456046678f1556c85d1adec4909bea70
edav03 <edavalc03@hotmail.com>
2022-01-20 15:18:15 +0100

    Actualizacion Clases

--- Programacion/U6_ProgamacionObjectes/Act2_3/Actividad2.java
+++ Programacion/U6_ProgamacionObjectes/Act2_3/Actividad2.java
@@ -1,6 +1,33 @@
 public class Actividad2{
     public static void main(String[] args){
-        Cotxe c1;
-        c1 = new Cotxe();
+        Cotxe c1 = new Cotxe();
+
+        System.out.println("Matricula: " + c1.atributos());
+        
+    }
+}
+
+class Cotxe {
+
+    String modelo;
+    String color;
+    boolean met;
+    int matricula;
+    String cotxe;
+    int fabricacion;
+    String modalidad;
+
+    Cotxe() {
+        modelo = "209";
+        color = "Red";
+        met = false;
+        matricula = 6693;
+        cotxe = "Mercedes";
+        fabricacion = 2009;
+        modalidad = "tercers";
+    }
+
+    public int atributos() {
+        return matricula;
     }
 }
