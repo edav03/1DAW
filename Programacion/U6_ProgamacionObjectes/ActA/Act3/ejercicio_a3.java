de96143a4f5ba4db9f9aa940ca6f0acce555a7df
Vidaved <edavalc03@hotmail.com>
2022-02-03 21:11:57 +0100

    Actualizacion Clases

--- Programacion/U6_ProgamacionObjectes/ActA/Act3/ejercicio_a3.java
+++ Programacion/U6_ProgamacionObjectes/ActA/Act3/ejercicio_a3.java
@@ -1,6 +1,14 @@
 public class ejercicio_a3{
     public static void main(String[] args){
 
+        rectangulo r1 = new rectangulo();
+        rectangulo r2 = new rectangulo();
+
+        r1.x1 = 0; r1.y1 = 0; r1.x2 = 5; r1.y2 = 5;
+        r2.x1 = 7; r2.y1 = 9; r2.x2 = 2; r2.y2 = 3;
+
+        System.out.println("Primero rectangulo:\n Perimetro = " + r1.perimetro() + "\n Area = " + r1.area());
+        System.out.println("Segundo rectangulo:\n Perimetro = " + r2.perimetro() + "\n Area = " + r2.area());
     }
 
 }
--- Programacion/U6_ProgamacionObjectes/ActA/Act3/rectangulo.java
+++ Programacion/U6_ProgamacionObjectes/ActA/Act3/rectangulo.java
@@ -1,18 +1,22 @@
 public class rectangulo {
 
-    int x1;
-    int x2;
-    int y1;
-    int y2;
+    int x1, x2, y1, y2;
 
+    public int ladoX(){
+        return(x2 - x1);
+    }
+
+    public int ladoY(){
+        return (y2 - y1);
+    }
 
     public int perimetro(){
-        int perimetro = (x2 - x1) + (y2 - y1);
+        int perimetro = ladoX() + ladoY();
         return perimetro;
     }
 
     public int area(){
-        int area = (x2 - x1) + (y2 - y1);
+        int area = ladoX() * ladoY();
         return area;
     }
 }
