d712d5007596283e741c24992f128aefa945e281
edav03 <edavalc03@hotmail.com>
2022-02-16 15:38:26 +0100

    Actualizacion Clases

--- Programacion/U6_ProgamacionObjectes/ActB/Act3/coordenadas.java
+++ Programacion/U6_ProgamacionObjectes/ActB/Act3/coordenadas.java
@@ -5,27 +5,32 @@ public class coordenadas {
     int x2;
     int y2;
     
-    public int ladox(){
-        int a = (x2 - x1);
-        return a;
-    }
-
-    public int ladoy(){
-        int b = (y2 - y1);
-        return b;
-    }
     public coordenadas(int x1, int y1, int x2, int y2){
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
+        if(x1 < x2 && y1 > y2){
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        } else{
+            System.err.println("Error al instanciar el rectangulo");
+        }
     }
 
-    public boolean hipotenusa(){
-        double hip = Math.sqrt((Math.pow(2, ladox())+ Math.pow(2, ladoy())));
+    public int ladoX(){
+        return(x2 - x1);
+    }
 
-        boolean al = true;
+    public int ladoY(){
+        return (y2 - y1);
+    }
 
-        return al;
+    public int perimetro(){
+        int perimetro = ladoX() + ladoY();
+        return perimetro;
+    }
+
+    public int area(){
+        int area = ladoX() * ladoY();
+        return area;
     }
 }
