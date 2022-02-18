b89e9fa8b8909af3f02741aa2493e6c75dca9a95
Vidaved <edavalc03@hotmail.com>
2022-02-18 17:39:56 +0100

    Actualizacion Clases

--- Programacion/U6_ProgamacionObjectes/bank_daw/CuentaBancaria.java
+++ Programacion/U6_ProgamacionObjectes/bank_daw/CuentaBancaria.java
@@ -1,16 +1,36 @@
 public class CuentaBancaria {
 
     private String iban, titular;
-    private double salario;
+    private double saldo;
 
-    public CuentaBancaria(String iban, String titular, double salario){
+    public CuentaBancaria(String iban, String titular, double saldo){
         if(iban.chars().count() < 24){
             System.err.println("Error al instanciar el IVA");
         } else{
             this.iban = iban;
             this.titular = titular;
-            this.salario = salario;
+            this.saldo = saldo;
         }
     }
-    
+
+    public String getiban(){
+        return iban;
+    }
+
+    public String gettitular(){
+        return titular;
+    }
+
+    public double getsaldo(){
+        return saldo;
+    }
+
+    public void setsaldo(double saldoX){
+        if(saldoX > 3000){
+            System.out.println("AVISO: Notificar a hacienda");
+        }
+
+        this.saldo += saldoX;
+    }
+
 }
--- Programacion/U6_ProgamacionObjectes/bank_daw/DawBank.java
+++ Programacion/U6_ProgamacionObjectes/bank_daw/DawBank.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class DawBank{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        CuentaBancaria c1 = new CuentaBancaria("ES6621000418401234567891", "Edison David Alcocer Valencia", 43080.54);
+        CuentaBancaria c1 = new CuentaBancaria("ES6621000418401234567891", "Edison David Alcocer Valencia", 0);
 
         int x = -1;
         String menu = 
@@ -17,8 +17,41 @@ public class DawBank{
         "\n8 SALIR";
 
         while(x != 8){
+            double n = 0;
             System.out.println(menu);
             x = sc.nextInt();
+            
+            switch(x){
+                case 1: System.out.println("IBAN: " + c1.getiban() + "\nTitular: " + c1.gettitular() + "\nSaldo: " + c1.getsaldo() + " €");
+                break;
+                case 2: System.out.println("IBAN: " + c1.getiban());
+                break;
+                case 3: System.out.println("Titular: " + c1.gettitular());
+                break;
+                case 4: System.out.println("Saldo: " + c1.getsaldo());
+                break;
+                case 5:
+                        System.out.println("Cantidad a ingresar: ");
+                        n = sc.nextDouble();
+                        if(n > 0){
+                            c1.setsaldo(n);
+                        } else{
+                            System.out.print("La cantidad debe ser superior a 0");
+                        }
+                        
+                break;
+                case 6: System.out.println("Cantidad a retirar: ");
+                        n = sc.nextDouble();
+                        if(n > 0){
+                            c1.setsaldo(n * -1);
+                        }else{
+                            System.out.print("La cantidad debe ser superior a 0");
+                        }
+                break;
+                case 7:
+                break;
+                default:
+            }
         }
 
     }
