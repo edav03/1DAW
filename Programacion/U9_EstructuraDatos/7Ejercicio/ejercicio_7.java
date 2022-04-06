adca9c7fb2a0e71844d8652d4cba3f17f3ee1103
Vidaved <edavalc03@hotmail.com>
2022-04-06 18:08:29 +0200

    Actualizacion Clases

--- Programacion/U9_EstructuraDatos/7Ejercicio/ejercicio_7.java
+++ Programacion/U9_EstructuraDatos/7Ejercicio/ejercicio_7.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class ejercicio_7 {
     public static void main(String[] args){
-        
+        ArrayList list = new ArrayList();
+
+        for(int i = 0; i < 6; i++){
+            list.add(new moneda());
+        }
+
+        for(Iterator<moneda> it = list.iterator(); it.hasNext();){
+            moneda m = (moneda)it.next();
+            System.out.println(m.getValor() + " -- " + m.getPos());
+        }
     }
 }
--- Programacion/U9_EstructuraDatos/7Ejercicio/moneda.java
+++ Programacion/U9_EstructuraDatos/7Ejercicio/moneda.java
@@ -1,10 +1,46 @@
+import java.util.Random;
+
 public class moneda {
 
-    private int valor;
-    private String pos;
+    private String valor, pos;
 
-    public moneda(int valor, String pos){
+    public moneda(){
+        Random rand = new Random();
+
+        String[] val = new String[8];
+        val[0] = "1 centimos";
+        val[1] = "2 centimos";
+        val[2] = "5 centimos";
+        val[3] = "10 centimos";    
+        val[4] = "25 centimos";
+        val[5] = "50 centimos";
+        val[6] = "1 Euro";
+        val[7] = "2 Euro";
+
+        int v = rand.nextInt(8);
+        this.valor = val[v];
+
+        int p = rand.nextInt(2);
+        if(p == 1){
+            this.pos = "cara";
+        }else{
+            this.pos = "cruz";
+        }
+    }
+
+    public String getValor(){
+        return valor;
+    }
+
+    public String getPos(){
+        return pos;
+    }
+
+    public void setValor(String valor){
         this.valor = valor;
+    }
+
+    public void setPos(String pos){
         this.pos = pos;
     }
 }
